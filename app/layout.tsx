6919afc997d834456582d86f234d7bab5a5fd10a
Igor Stadnyk <i@inc4.net>
2024-07-05 19:38:44 +0200

    Initial

---
+++ app/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Facterless",
+  description: "Facterless App",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body suppressHydrationWarning={true} className={inter.className}>
+        {children}
+      </body>
+    </html>
+  );
+}
